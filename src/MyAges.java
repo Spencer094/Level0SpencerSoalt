4497ef25f3ce0c992b0f6945f5f5cb920410e9d9
Spencer094 <league@Mac-mini.local>
2016-03-02 14:58:08 -0800

    3/2/16
    The work i did today.

---
+++ src/MyAges.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class MyAges {
+public static void main(String[] args) {
+	for (int i = 0; i < 15; i++) {
+		JOptionPane.showMessageDialog(null, "I Was");
+		
+	}
+	
+	}
+}
+
---
+++ src/badgers.java
@@ -0,0 +1,16 @@
+
+public class badgers {
+public static void main(String[] args) {
+	for (int i = 0; i < 2; i++) {
+		
+	
+	for (int i = 0; i < 12; i++) {
+		System.out.println("Badgers");
+	}
+for (int i = 0; i < 2; i++) {
+	System.out.println("Mushroom");
+	{
+}
+}
+
+}
